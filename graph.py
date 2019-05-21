0401f6eab04ecbb99c2def8d2c0a82b974f55604
Basile Tousside <basile.tousside@hs-bochum.de>
2019-05-21 15:04:47 +0200

    modified graph.py

--- graph.py
+++ graph.py
@@ -1,11 +1,16 @@
 import tensorflow as tf
 
-@tf.function
-def demo(x,y):
-	return x*x*y + y + 2
-
 
 x = tf.Variable(3, name="x")
 y = tf.Variable(4, name="y")
+f = x*x*y + y + 2
+print(f.eval)
 
-r = demo(x,y)
+
+w = tf.constant(3)
+x = w + 2
+y = x + 5
+z = x*3
+
+print(y.eval)
+print(z.eval)
