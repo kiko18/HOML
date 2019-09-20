c4edbe05e808921646b5c6125376e7ac175f21eb
Basil <50740026+kiko18@users.noreply.github.com>
2019-09-20 06:42:33 +0200

    Update batch_norm.py

--- DL/batch_norm.py
+++ DL/batch_norm.py
@@ -12,17 +12,26 @@ they won't come back during traning.
 In a 2015 paper, Sergey Ioffe and Christian Szegedy proposed a technique called Batch normalization (BN)
 to adress the vanishing/exploding gradient problems, and more generally the problem that the distribution
 of each layer's inputs changes during training, as the parameters of the previous layers change (which
-they call Internal Covsriate Shift problem).
+they call Internal Covariante Shift problem).
 
 The technique consists of adding an operation in the model just before the activation function of each layer.
 Simply zero-centering and normalizing the inputs, then scaling and shifting the result using 2 new parameters
 per layer (one for scaling the other for shifting). In others words, this operation lets the model learn 
 the optimal scale and mean of the inputs for each layer.
+
+The process is as follow:
+1- compute the mean
+2- compute the standart deviation
+3- compute the zero-centered and normalized inputs
+4- scale and shift (offset) the normalized input
+
+So, in total, 4 parameters are learned for each batch-normalized layer: scale, offset, mean and 
+standart deviation.
 '''
 
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 from functools import partial
 
@@ -39,20 +48,25 @@ n_hidden1 = 300
 n_hidden2 = 100
 n_outputs = 10
 
-
-#Let's build a neural net for MNIST, using the ELU activation function and Batch Normalization at each layer:
-reset_graph()
-
-batch_norm_momentum = 0.9
+#The batch-normalization algorithm uses exponential decay to compute the running averages
+#which is why it requires the momentum parameter. Given a new value v, the running average v_hat
+#is updated through the equation: v_hat <- v_hat * momentum + v * (1-momentum)
+batch_norm_momentum = 0.9   #a good momentum value is typically close to one
 learning_rate = 0.01
 
 X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
 y = tf.placeholder(tf.int32, shape=(None), name="y")
+
+#define a training placehoder that will be set to True during training and to false otherwise 
+#to tell the tf.layers.batch_normalization() function wheter it should use the current 
+#mini-batch's mean and standard deviation (during training) or the whole set's mean and standard deviation 
+#(during testing)
 training = tf.placeholder_with_default(False, shape=(), name='training')
 
 with tf.name_scope("dnn"):
     he_init = tf.variance_scaling_initializer()
 
+    #To avoid repeating the same parameters over and over again, we can use Python's partial() function:
     my_batch_norm_layer = partial(
             tf.layers.batch_normalization,
             training=training,
@@ -63,10 +77,12 @@ with tf.name_scope("dnn"):
             kernel_initializer=he_init)
 
     hidden1 = my_dense_layer(X, n_hidden1, name="hidden1")
-    bn1 = tf.nn.elu(my_batch_norm_layer(hidden1))
-    hidden2 = my_dense_layer(bn1, n_hidden2, name="hidden2")
-    bn2 = tf.nn.elu(my_batch_norm_layer(hidden2))
-    logits_before_bn = my_dense_layer(bn2, n_outputs, name="outputs")
+    bn1 = my_batch_norm_layer(hidden1)
+    bn1_act = tf.nn.elu(bn1)
+    hidden2 = my_dense_layer(bn1_act, n_hidden2, name="hidden2")
+    bn2 = my_batch_norm_layer(hidden2)
+    bn2_act = tf.nn.elu(bn2)
+    logits_before_bn = my_dense_layer(bn2_act, n_outputs, name="outputs")
     logits = my_batch_norm_layer(logits_before_bn)
 
 '''
@@ -77,19 +93,22 @@ with tf.name_scope("loss"):
     loss = tf.reduce_mean(xentropy, name="loss")
 
 '''
-Define a gradient optimizer
+Create a gradient optimizer and tell it to minimize the cost
 '''
 with tf.name_scope("train"):
     optimizer = tf.train.GradientDescentOptimizer(learning_rate)
     training_op = optimizer.minimize(loss)
 
 '''
-Define the evaluation operator
+Define the evaluation operation
 '''
 with tf.name_scope("eval"):
     correct = tf.nn.in_top_k(logits, y, 1)
     accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
     
+'''
+create a variable initializer as well as a saver
+'''
 init = tf.global_variables_initializer()
 saver = tf.train.Saver()
 
@@ -122,6 +141,12 @@ we need to explicitly run the extra update operations needed by batch normalizat
 n_epochs = 20
 batch_size = 200
 
+'''
+Batch normalization() creates a few operations that must be evaluated at each step during training in 
+order to update the mooving averages (recall that these moving averages are needed to evaluate the trainig 
+set's mean and standard deviation). These operation (bachnorm add) are automatically added to the UPDATE_OPS collection,
+so all we need to do is get the list of operations in that collection and run them at each training iteration.
+'''
 extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 
 modelParamsDir = 'C:/Users/BT/Documents/others/tf/tf_boards/params/mlp_plain_tf.ckpt'
@@ -130,13 +155,23 @@ with tf.Session() as sess:
     init.run()
     for epoch in range(n_epochs):
         for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
-            sess.run([training_op, extra_update_ops],
-                     feed_dict={training: True, X: X_batch, y: y_batch})
+            #training_op depend on batch_normalization(), this implies we need to set training placeholder to true
+            sess.run([training_op, extra_update_ops], feed_dict={training: True, X: X_batch, y: y_batch})
         accuracy_val = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
         print(epoch, "Validation accuracy:", accuracy_val)
 
     save_path = saver.save(sess, modelParamsDir)
     
+'''
+And that's all! In this tiny example with just two layers, it's unlikely that batch normalization will have 
+a very positive impact, but for deeper networks it can make a tremendous difference.
+
+One more thing: notice that the list of trainable variables is shorter than the list of all global variables. 
+This is because the moving averages are non-trainable variables. 
+If you want to reuse a pretrained neural network (see below), you must not forget these non-trainable variables.
+'''    
+[v.name for v in tf.trainable_variables()]
+[v.name for v in tf.global_variables()]
     
 '''
 
